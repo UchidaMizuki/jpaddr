bc72f83d998a77e5f5168e271512842e57658db7
Uchida Mizuki <uchidamizuki@vivaldi.net>
2023-12-02 20:05:08 +0900

    Update `normalize_address`

--- R/normalize_address.R
+++ R/normalize_address.R
@@ -3,9 +3,6 @@
 #' @param address Address to normalize.
 #' @param level Level of address normalization. One of `"town"`, `"city"`, or
 #' `"pref"` (by default, `"town"`).
-#' @param pause Pause time between requests in seconds (by default, `0.1`).
-#' @param timeout Timeout for each request in seconds (by default, `10`).
-#' @param progress Whether to show progress bar (by default, `FALSE`).
 #'
 #' @return A tibble with normalized address.
 #'
@@ -13,15 +10,7 @@
 #'
 #' @export
 normalize_address <- function(address,
-                              level = "town",
-                              pause = 0.1,
-                              timeout = 10,
-                              progress = FALSE) {
-  if (pause < 0.1) {
-    rlang::warn("`pause` is too short. Set to 0.1 seconds.")
-    pause <- 0.1
-  }
-
+                              level = "town") {
   level <- rlang::arg_match(level, c("town", "city", "pref"),
                             multiple = TRUE) |>
     dplyr::case_match("town" ~ 3,
@@ -30,37 +19,19 @@ normalize_address <- function(address,
 
   data <- tibble::tibble(address = vctrs::vec_cast(address, character()),
                          level = vctrs::vec_cast(level, integer()))
+  data_unique <- vctrs::vec_unique(data)
 
   file <- fs::path(system.file("normalize-japanese-addresses", package = "jpaddr"), "normalize_address.json")
-  on.exit(if(fs::file_exists(file)) fs::file_delete(file))
+  jsonlite::write_json(data_unique, file)
+  on.exit(fs::file_delete(file))
 
-  data_unique <- vctrs::vec_unique(data) |>
-    dplyr::mutate(data = purrr::map2(
-      address, level,
-      purrr::slowly(purrr::possibly(\(address, level) {
-        processx::run(command = "node",
-                      args = c("normalize_address.js", address, level, timeout),
-                      wd = system.file("normalize-japanese-addresses",
-                                       package = "jpaddr"))
+  processx::run(command = "node",
+                args = "normalize_address.js",
+                wd = system.file("normalize-japanese-addresses",
+                                 package = "jpaddr"))
 
-        out <- jsonlite::read_json(file)
-        out$lat <- out$lat %||% NA_real_
-        out$lng <- out$lng %||% NA_real_
-        out$error <- FALSE
-        out
-      },
-      otherwise = tibble::tibble(pref = NA_character_,
-                                 city = NA_character_,
-                                 town = NA_character_,
-                                 addr = NA_character_,
-                                 level = NA_integer_,
-                                 lat = NA_real_,
-                                 lng = NA_real_,
-                                 error = TRUE)),
-      rate = purrr::rate_delay(pause)),
-      .progress = progress
-    ) |>
-      dplyr::bind_rows())
+  data_unique <- data_unique |>
+    dplyr::mutate(data = tibble::as_tibble(jsonlite::fromJSON(file)))
 
   data |>
     dplyr::left_join(data_unique,
@@ -72,5 +43,7 @@ normalize_address <- function(address,
                                             1 ~ "pref",
                                             2 ~ "city",
                                             3 ~ "town") |>
-                    factor(c("pref", "city", "town")))
+                    factor(c("pref", "city", "town")),
+                  dplyr::across(c("lat", "lng"),
+                                as.double))
 }
--- inst/normalize-japanese-addresses/normalize_address.js
+++ inst/normalize-japanese-addresses/normalize_address.js
@@ -3,16 +3,11 @@ const fs = require('fs');
 
 config.japaneseAddressesApi = 'file:///japanese-addresses/api/ja';
 
-const timeout = process.argv[4] * 1000;
+async function normalize_address() {
+  const inputs = JSON.parse(fs.readFileSync('normalize_address.json', 'utf8'));
+  const outputs = await Promise.all(inputs.map(input => normalize(input['address'], { level: input['level'] })));
 
-const timer = setTimeout(() => {
-  process.exit(1);
-}, timeout);
+  await fs.writeFileSync('normalize_address.json', JSON.stringify(outputs));
+}
 
-normalize(
-  process.argv[2],
-  { level: process.argv[3] }
-).then(result => {
-  clearTimeout(timer);
-  fs.writeFileSync('normalize_address.json', JSON.stringify(result));
-});
+normalize_address();
